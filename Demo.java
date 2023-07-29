88637d4c9468bca70d9699867425ead1651150ab
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-07-29 22:48:12 +0530

    Second commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,11 @@
 public class Demo{
     public static void main(String[] args) {
-        
+        float bankBalance = 123456.7890f;
+        System.out.printf("$%,.2f\n",bankBalance);
+
+        float PI = (float)Math.PI;
+        System.out.printf("%.3f\n",PI);
+
+
     }
 }
