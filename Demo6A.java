077a46fb52d814a86b70e7deeb90523410ce43fb
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-07-31 21:25:20 +0530

    Complete Task 6-A

---
+++ Demo6A.java
@@ -0,0 +1,8 @@
+public class Demo6A {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 0;
+
+        System.out.printf("[\033[41m%5d\033[42m%-5d\033[0m]",num1,num2);
+    }
+}
